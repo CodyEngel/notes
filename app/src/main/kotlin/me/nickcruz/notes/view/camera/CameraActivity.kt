5d717fdb4d45f8a313879e50610dcf056bd684e4
Nick Cruz <ncruz@umich.edu>
2017-07-22 11:55:15 -0500

    Configured fotoapparat into app.

--- app/src/main/kotlin/me/nickcruz/notes/view/camera/CameraActivity.kt
+++ app/src/main/kotlin/me/nickcruz/notes/view/camera/CameraActivity.kt
@@ -3,16 +3,20 @@ package me.nickcruz.notes.view.camera
 import android.arch.lifecycle.LifecycleActivity
 import android.content.Context
 import android.content.Intent
-import android.graphics.SurfaceTexture
 import android.os.Bundle
-import android.util.Log
-import android.view.TextureView
+import android.view.View
+import android.widget.ImageView
+import io.fotoapparat.Fotoapparat
+import io.fotoapparat.parameter.LensPosition
+import io.fotoapparat.parameter.ScaleType
+import io.fotoapparat.parameter.selector.*
+import io.fotoapparat.result.transformer.SizeTransformers
 import kotlinx.android.synthetic.main.activity_camera.*
 import kotlinx.android.synthetic.main.content_camera.*
 import me.nickcruz.notes.R
 import me.nickcruz.notes.model.Note
-import me.nickcruz.notes.view.attachToLifecycle
-import me.nickcruz.notes.view.camera.permission.CameraPermissionChecker
+import me.nickcruz.notes.view.camera.permissions.PermissionsDelegate
+import java.io.File
 
 
 /**
@@ -24,47 +28,97 @@ class CameraActivity : LifecycleActivity() {
         val EXTRA_NOTE = "note"
 
         fun getStartIntent(context: Context, note: Note) =
-            Intent(context, CameraActivity::class.java).apply { putExtra(EXTRA_NOTE, note) }
+                Intent(context, CameraActivity::class.java).apply { putExtra(EXTRA_NOTE, note) }
     }
 
-    private val cameraPermissionChecker = CameraPermissionChecker(this)
+    val permissionsDelegate = PermissionsDelegate(this)
+    var hasCameraPosition = false
+
+    private var fotoapparat: Fotoapparat? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_camera)
         setActionBar(toolbar)
 
-        openCamera()
-    }
+        hasCameraPosition = permissionsDelegate.hasCameraPermission()
 
-    override fun onResume() {
-        super.onResume()
-
-        if (autoFitTextureView.isAvailable) {
-            openCamera()
+        if (hasCameraPosition) {
+            cameraView.visibility = View.VISIBLE
         } else {
-            autoFitTextureView.surfaceTextureListener = object : TextureView.SurfaceTextureListener {
-                override fun onSurfaceTextureSizeChanged(surface: SurfaceTexture?, width: Int, height: Int) {
-                }
+            permissionsDelegate.requestCameraPermission()
+        }
 
-                override fun onSurfaceTextureUpdated(surface: SurfaceTexture?) {
-                }
-
-                override fun onSurfaceTextureDestroyed(surface: SurfaceTexture?): Boolean {
-                    return false
-                }
-
-                override fun onSurfaceTextureAvailable(surface: SurfaceTexture?, width: Int, height: Int) {
-                    openCamera()
-                }
-            }
+        createFotoapparat()
+        cameraView.setOnClickListener { takePicture() }
+        cameraView.setOnLongClickListener {
+            fotoapparat?.autoFocus()
+            true
         }
     }
 
-    private fun openCamera() {
-        cameraPermissionChecker.checkPermission()
-                .andThen(NotesCamera(this).open())
-                .subscribe({}, { Log.e("CameraTest", it.message) })
-                .attachToLifecycle(this)
+    override fun onStart() {
+        super.onStart()
+        if (hasCameraPosition) {
+            fotoapparat?.start()
+        }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        if (hasCameraPosition) {
+            fotoapparat?.stop()
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (permissionsDelegate.resultGranted(requestCode, permissions as Array<String>, grantResults)) {
+            fotoapparat?.start()
+        }
+    }
+
+    private fun createFotoapparat() {
+        fotoapparat = Fotoapparat
+                .with(this)
+                .into(cameraView)
+                .previewScaleType(ScaleType.CENTER_CROP)
+                .photoSize(AspectRatioSelectors.standardRatio(SizeSelectors.biggestSize()))
+                .lensPosition(LensPositionSelectors.lensPosition(LensPosition.BACK))
+                .focusMode(Selectors.firstAvailable(
+                        FocusModeSelectors.continuousFocus(),
+                        FocusModeSelectors.autoFocus(),
+                        FocusModeSelectors.fixed()
+                ))
+                .flash(Selectors.firstAvailable(
+                        FlashSelectors.autoRedEye(),
+                        FlashSelectors.autoFlash(),
+                        FlashSelectors.torch(),
+                        FlashSelectors.off()
+                ))
+//                .frameProcessor(SampleFrameProcessor())
+//                .logger(loggers(
+//                        logcat(),
+//                        fileLogger(this)
+//                ))
+                .build()
+    }
+
+    private fun takePicture() {
+        val photoResult = fotoapparat?.takePicture()
+
+        photoResult?.saveToFile(File(
+                getExternalFilesDir("photos"),
+                "photo.jpg"
+        ))
+
+        photoResult
+                ?.toBitmap(SizeTransformers.scaled(0.25f))
+                ?.whenAvailable({ result ->
+                    val imageView = findViewById(R.id.result) as ImageView
+
+                    imageView.setImageBitmap(result.bitmap)
+                    imageView.rotation = (-result.rotationDegrees).toFloat()
+                })
     }
 }
---
+++ app/src/main/kotlin/me/nickcruz/notes/view/camera/permissions/PermissionsDelegate.kt
@@ -0,0 +1,62 @@
+package me.nickcruz.notes.view.camera.permissions
+
+import android.Manifest
+import android.app.Activity
+import android.content.pm.PackageManager
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
+
+/**
+ * Created by Nick Cruz on 7/22/17
+ */
+class PermissionsDelegate(private val activity: Activity) {
+
+    companion object {
+        private val REQUEST_CODE = 10
+    }
+
+    internal fun hasCameraPermission(): Boolean {
+        val permissionCheckResult = ContextCompat.checkSelfPermission(
+                activity,
+                Manifest.permission.CAMERA
+        )
+        return permissionCheckResult == PackageManager.PERMISSION_GRANTED
+    }
+
+    internal fun requestCameraPermission() {
+        ActivityCompat.requestPermissions(
+                activity,
+                arrayOf(Manifest.permission.CAMERA),
+                REQUEST_CODE
+        )
+    }
+
+    internal fun resultGranted(requestCode: Int,
+                               permissions: Array<String>,
+                               grantResults: IntArray): Boolean {
+
+        if (requestCode != REQUEST_CODE) {
+            return false
+        }
+
+        if (grantResults.isEmpty()) {
+            return false
+        }
+
+        if (permissions[0] != Manifest.permission.CAMERA) {
+            return false
+        }
+
+        //        View noPermissionView = activity.findViewById(R.id.no_permission);
+
+        if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            //            noPermissionView.setVisibility(View.GONE);
+            return true
+        }
+
+        requestCameraPermission()
+        //        noPermissionView.setVisibility(View.VISIBLE);
+        return false
+    }
+}
+
